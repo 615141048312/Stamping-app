0e2501329e86343a3a5803a2d73bae298486f1c8
615141048312 <hanada.yui@mbr.nifty.com>
2021-01-25 17:38:43 +0900

    Change class name Calc from Access

--- src/saveDate/Access.java
+++ src/saveDate/Access.java
@@ -8,11 +8,16 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Date;
 
-public class Calc {
+public class Access {
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/save_date?user=user01&password=password01&useSSL=false";
 	private static final String SQL = "select stamping from stampings where id = ?";
 	public AtWork today = new AtWork();
+	public PreparedStatement statement1;
+	public PreparedStatement statement2;
+	public Date Date1;
+	public Date Date2;
+
 
 	public AtWork stamps() throws ClassNotFoundException {
 
@@ -24,8 +29,10 @@ public class Calc {
 		}
 
 		try (Connection connection = DriverManager.getConnection(URL);
-				PreparedStatement statement1 = connection.prepareStatement(SQL);
-				PreparedStatement statement2 = connection.prepareStatement(SQL)) {
+				) {
+			statement1 = connection.prepareStatement(SQL);
+			statement2 = connection.prepareStatement(SQL);
+
 			statement1.setLong(1, 1L);
 			statement2.setLong(1, 2L);
 
@@ -35,15 +42,18 @@ public class Calc {
 			ResultSet rs1 = statement1.getResultSet();
 			ResultSet rs2 = statement2.getResultSet();
 
-			// Date Date1 = new Date(rs1.getTime());
-			// Date Date2 = new Date(rs2.getTime());
+			Timestamp ts1 = rs1.getTimestamp("stamping");
+			Timestamp ts2 = rs2.getTimestamp("stamping");
+
+			Date1 = new Date(ts1.getTime());
+			Date2 = new Date(ts2.getTime());
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
-		// today.start = ;
-		// today.leave = ;
+		today.start = Date1;
+		today.leave = Date2;
 		return today;
 	}
 }
